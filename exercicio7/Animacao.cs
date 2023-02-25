e4cb843bd9d5462c1d31a969f1b0fe5218615cf5
Vanessa Monteiro <vanessascmonteiro@gmail.com>
2023-02-25 19:40:30 -0300

    exercicio7

---
+++ exercicio7/Animacao.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio7
+{
+    public static class Animacao
+    {
+      
+    public static void animacao()
+    {
+       Console.WriteLine("Animacao Frozen"); 
+    }
+    public static void animacao(string tipo){
+      Console.WriteLine($"Animacao {tipo}"); 
+    } 
+
+    }
+
+}
---
+++ exercicio7/Program.cs
@@ -0,0 +1,11 @@
+﻿using exercicio7;
+
+public class Program
+{
+    public static void Main(string[] args){
+    Animacao.animacao();
+    Animacao.animacao("Sherek");
+
+    }
+}
+
